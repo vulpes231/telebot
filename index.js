8e7f48ab2ab9aae3ac742afb18915b7b4f5a2936
Vulpes231 <whykayshot@gmail.com>
2024-06-13 12:39:50 +0100

    check check

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 // Replace with your bot token
 const token = process.env.TELEGRAM_TOKEN;
-const bot = new TelegramBot(token, { polling: false }); // Set polling to false for webhook mode
+const bot = new TelegramBot(token, { polling: false });
 
 // Express server setup
 const app = express();
@@ -14,11 +14,19 @@ app.use(bodyParser.json());
 const packages = {
   signal: {
     price: 50,
-    wallet: "WALLET_ADDRESS_FOR_SIGNAL",
+    wallet: {
+      usdt: "0x59a7C8b85Ea3d4f2056dae28343d1538C87FF63A",
+      btc: "3LotQGWvGvjKbTszKs3PQBeQhbCeLxqnS1",
+      eth: "0x59a7C8b85Ea3d4f2056dae28343d1538C87FF63A",
+    },
   },
   mentorship: {
     price: 200,
-    wallet: "WALLET_ADDRESS_FOR_MENTORSHIP",
+    wallet: {
+      usdt: "0x59a7C8b85Ea3d4f2056dae28343d1538C87FF63A",
+      btc: "3LotQGWvGvjKbTszKs3PQBeQhbCeLxqnS1",
+      eth: "0x59a7C8b85Ea3d4f2056dae28343d1538C87FF63A",
+    },
   },
 };
 
@@ -33,19 +41,25 @@ bot.onText(/\/start/, (msg) => {
 // Signal package command
 bot.onText(/\/signal/, (msg) => {
   const packageInfo = packages.signal;
-  bot.sendMessage(
-    msg.chat.id,
-    `You chose the signal package. Please pay $${packageInfo.price} to wallet ${packageInfo.wallet}.`
-  );
+  const responseMessage = `
+You chose the signal package. Please pay $${packageInfo.price} to one of the following wallets:
+USDT: ${packageInfo.wallet.usdt}
+BTC: ${packageInfo.wallet.btc}
+ETH: ${packageInfo.wallet.eth}
+  `;
+  bot.sendMessage(msg.chat.id, responseMessage);
 });
 
 // Mentorship package command
 bot.onText(/\/mentorship/, (msg) => {
   const packageInfo = packages.mentorship;
-  bot.sendMessage(
-    msg.chat.id,
-    `You chose the mentorship package. Please pay $${packageInfo.price} to wallet ${packageInfo.wallet}.`
-  );
+  const responseMessage = `
+You chose the mentorship package. Please pay $${packageInfo.price} to one of the following wallets:
+USDT: ${packageInfo.wallet.usdt}
+BTC: ${packageInfo.wallet.btc}
+ETH: ${packageInfo.wallet.eth}
+  `;
+  bot.sendMessage(msg.chat.id, responseMessage);
 });
 
 // Express webhook route
